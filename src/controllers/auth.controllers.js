0e810e42c073c99adcf885ac3f1ac57b42382934
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2024-09-10 17:23:50 -0600

    token creation

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,6 @@
 import User from "../models/user.models.js";
+import bcrypt from "bcryptjs"; // para encriptar las passwords
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   // obtener datos
@@ -6,21 +8,35 @@ export const register = async (req, res) => {
 
   //guardar el objecto en la db
   try {
+    //encriptar la password
+    const passwordHash = await bcrypt.hash(password, 10);
+
     // crear un usuario
     // instanciar un objeto
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
-    // console.log(newUser);
 
+    //se guarda el dato
     const userSaved = await newUser.save();
 
+    // token
+    // guardar el token
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+
     // manda el dato al frontend
-    res.send(userSaved);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updateAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
 export const login = (req, res) => {
