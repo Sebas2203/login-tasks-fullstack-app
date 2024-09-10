ec8c62a9ae274b79820ce3441434658836adaa7a
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2024-09-10 17:40:32 -0600

    login and logout

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -39,6 +39,42 @@ export const register = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-export const login = (req, res) => {
-  res.send("login");
+
+export const login = async (req, res) => {
+  // obtener datos
+  const { email, password } = req.body;
+
+  //guardar el objecto en la db
+  try {
+    //comparar la password
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect password" });
+
+    // crear el token
+    const token = await createAccessToken({ id: userFound._id });
+    res.cookie("token", token);
+
+    // manda el dato al frontend
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updateAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
 };
